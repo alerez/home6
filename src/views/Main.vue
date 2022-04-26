178d342ffa18c1e490f4e4fd5a736ac9796b1d65
Олександр Резніченко <alex.reznichenko2001@gmail.com>
2022-04-26 16:00:34 +0300

    init

---
+++ src/views/Main.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="page__main main">
+    <div class="main__container">
+      <div class="main__body">
+        <div class="main__description description">
+          <div class="description__body">
+            <div class="description__title">Авторское <br> строительство <br> современных <br> домов
+              <div class="description__button pcBtn">в спб и Ленинград- <br>
+              ской области</div></div>
+            <div class="description__button mobileBtn">в спб и Ленинград- <br>
+              ской области</div>
+          </div>
+        </div>
+
+        <div class="main__download download">
+          <div class="download__body">
+            <div class="download__text">{{ store.mainData.text }}</div>
+            <a class="download__button" :href="store.mainData.link">{{ store.mainData.button }}</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { useStore } from '@/store/index';
+
+const store = useStore();
+</script>
